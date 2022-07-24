c352a95aebe128e9f04b69c905afc4a4ee420375
rafa <rflernesto@gmail.com>
2022-07-24 14:45:52 -0400

    refactor: remove unused files

--- src/test/java/quarkus/bank/NativeBankResourceIT.java
+++
@@ -1,9 +0,0 @@
-package quarkus.bank;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeBankResourceIT extends BankResourceTest {
-
-    // Execute the same tests but in native mode.
-}
