19643eb384a88b5c1c687b22211894f470c2f74f
rafa <rflernesto@gmail.com>
2022-07-25 21:41:58 -0400

    feat: get support data

--- src/main/java/quarkus/bank/BankResource.java
+++ src/main/java/quarkus/bank/BankResource.java
@@ -1,16 +1,51 @@
 package quarkus.bank;
 
+import io.quarkus.arc.config.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Optional;
 
 @Path("/bank")
 public class BankResource {
 
+    @ConfigProperty(name="bank.name")
+    String name;
+
+    @ConfigProperty(name="bank.mobileBanking")
+    Optional<Boolean>  mobileBanking;
+
+    @Inject
+    BankSupportConfig bankSupportConfig;
+
     @GET
+    @Path("/name")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String getName() {
+        return name;
+    }
+
+    @GET
+    @Path("/mobilebanking")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean getMobileBanking() {
+        return mobileBanking.orElse(false);
+    }
+
+    @GET
+    @Path("/support")
+    @Produces(MediaType.APPLICATION_JSON)
+    public HashMap<String, String> getSupport() {
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("email", bankSupportConfig.email);
+        map.put("phone", bankSupportConfig.getPhone());
+
+        return map;
     }
 }
---
+++ src/main/java/quarkus/bank/BankSupportConfig.java
@@ -0,0 +1,19 @@
+package quarkus.bank;
+
+import io.quarkus.arc.config.ConfigProperties;
+
+@ConfigProperties(prefix = "bank-support")
+public class BankSupportConfig {
+    private String phone;
+    public String email;
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
